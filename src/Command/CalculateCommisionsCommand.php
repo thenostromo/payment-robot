1d157e0d7b5b6d48d3bc68cfea2bc8e68a2655b9
Alexander Bruk <a.bruk@belvg.com>
2022-11-21 19:51:26 +0300

    Application initialization

---
+++ src/Command/CalculateCommisionsCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'CalculateCommisionsCommand',
+    description: 'Add a short description for your command',
+)]
+class CalculateCommisionsCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('filePath', InputArgument::OPTIONAL, 'Enter the path to the file')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $filePath = $input->getArgument('filePath');
+
+        dd($filePath);
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
